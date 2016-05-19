90365ab6ce749e8b23b4e73494857531a37b05c4
lod <lod@bremsspur.org>
2016-05-20 01:11:42 +0200

    added tests from google plugin

--- test.py
+++ test.py
@@ -9,8 +9,18 @@ from supybot.test import *
 
 
 class SearxTestCase(PluginTestCase):
-    plugins = ('Searx',)
+    plugins = ('Searx','Config')
 
     def testSearx(self):
         self.assertNotError('searx google')
+
+    def testLucky(self):
+        self.assertResponse('lucky Hacker News',
+                    'https://news.ycombinator.com/')
+    def testSearchFormat(self):
+        self.assertRegexp('searx foo', '<https?://.*>')
+        self.assertNotError('config reply.format.url %s')
+        self.assertRegexp('searx foo', 'https?://.*')
+        self.assertNotRegexp('searx foo', '<https?://.*>')
+
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
